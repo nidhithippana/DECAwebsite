a7edb2732d71a0ea392fbf7be3a3d62001f3a992
nidhithippana <54454735+nidhithippana@users.noreply.github.com>
2019-10-30 15:51:48 -0500

    oof

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCMrIHGwmH4dcqwG_brfzESK3Mj-cF8N3Q",
-    authDomain: "index-753b9.firebaseapp.com",
-    databaseURL: "https://index-753b9.firebaseio.com",
-    projectId: "index-753b9",
-    storageBucket: "",
-    messagingSenderId: "564783210479",
-    appId: "1:564783210479:web:e30f32854dcab2f214e01a"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  
-  
-  function login(){
+$("#log_in").submit(function(e) {
 	  
-	  var email = document.getElementById("email").value;
-	  var password = document.getElementById("pass1").value;
+	  var email = document.getElementById("logemail_inp").value;
+	  var password = document.getElementById("logpassword_inp").value;
 	  
   	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
 	  // Handle Errors here.
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
+	  alert(errorCode, errorMessage)
+	  
 	  // ...
 	});
-  }
+  });
+
+firebase.auth().onAuthStateChanged(function(user){
+	if(user){
+	console.log(user);
+	document.getElementById("topnav").style.display="block";
+	
+		}
+	else{
+		console.log("not logged in");
+	}
+});
 
 
-
-
+/*function signup(){
 
 firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
   // Handle Errors here.
@@ -34,9 +33,11 @@ firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(e
   var errorMessage = error.message;
   // ...
 });
+}
 
-firebase.auth().signOut().then(function() {
+firebase.auth().signOut().then(function(error) {
   // Sign-out successful.
 }).catch(function(error) {
   // An error happened.
 });
+*/
