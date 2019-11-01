8a4b1578943e7a24c69a4e84554a141f65b55270
nidhithippana <54454735+nidhithippana@users.noreply.github.com>
2019-11-01 15:30:35 -0500

    upd

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-$("#log_in").submit(function(e) {
-	var email = document.getElementById("logemail_inp").value;
-	var password = document.getElementById("logpassword_inp").value;
-  	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		alert(errorCode, errorMessage)
-	});
+$("#log").submit( function(e) {
+e.preventDefault();
+var email = document.getElementById("logemail_inp").value;
+var password = document.getElementById("logpassword_inp").value;
+//signs a user in if they had already created a password with their email
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+var errorCode = error.code;
+var errorMessage = error.message;
+alert(errorCode, errorMessage);
+  // ...
+});
+document.getElementById("logemail_inp").value="";
+document.getElementById("logpassword_inp").value="";
 });
 
 firebase.auth().onAuthStateChanged(function(user){
@@ -30,7 +36,7 @@ firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(e
 */
 function signOut(){
 firebase.auth().signOut().then(function(error) {
-  // Sign-out successful.
+	window.location.reload();
 }).catch(function(error) {
   // An error happened.
 });
