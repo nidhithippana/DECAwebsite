bc9848ba5c8ed88b84477b9a09fdc7173546989c
nidhithippana <54454735+nidhithippana@users.noreply.github.com>
2019-11-05 16:02:02 -0600

    upd

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
 $("#log").submit( function(e) {
-e.preventDefault();
-var email = document.getElementById("logemail_inp").value;
-var password = document.getElementById("logpassword_inp").value;
-//signs a user in if they had already created a password with their email
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-var errorCode = error.code;
-var errorMessage = error.message;
-alert(errorCode, errorMessage);
-  // ...
+	e.preventDefault();
+	var email = document.getElementById("logemail_inp").value;
+	var password = document.getElementById("logpassword_inp").value;
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		alert(errorCode, errorMessage);
+	});
+	document.getElementById("logemail_inp").value="";
+	document.getElementById("logpassword_inp").value="";
 });
-document.getElementById("logemail_inp").value="";
-document.getElementById("logpassword_inp").value="";
+
+$("#signup").submit( function(e) {
+	e.preventDefault();
+	
+	var email = document.getElementById("signup_email").value;
+	var password = document.getElementById("signup_pass").value;
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		alert(errorCode, errorMessage);
+	});
+	document.getElementById("signup_email").value="";
+	document.getElementById("signup_pass").value="";
 });
+
 
 firebase.auth().onAuthStateChanged(function(user){
 	if(user){
 	console.log(user);
 	document.getElementById("topnav").style.display="block";
+	window.open("DECAMAIN.html","_self");
 	} else{
 		console.log("not logged in");
 	}
 });
 
 
-/*function signup(){
 
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-}
-*/
 function signOut(){
 firebase.auth().signOut().then(function(error) {
-	window.location.reload();
+	window.open("loginpg.html","_self");
 }).catch(function(error) {
   // An error happened.
 });
